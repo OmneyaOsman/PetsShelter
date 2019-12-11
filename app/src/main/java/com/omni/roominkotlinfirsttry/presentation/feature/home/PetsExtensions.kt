84cdaa94a57d6f6e23b21fd13fa07d00887eab28
Omni <engomneya.osman@gmail.com>
2019-12-11 23:55:38 +0200

    observe emptyView changes

--- app/src/main/java/com/omni/roominkotlinfirsttry/presentation/feature/home/PetsExtensions.kt
+++ app/src/main/java/com/omni/roominkotlinfirsttry/presentation/feature/home/PetsExtensions.kt
@@ -1,16 +1,16 @@
 package com.omni.roominkotlinfirsttry.presentation.feature.home
 
-//@BindingAdapter("app:payload")
-//fun RecyclerView.setItems( items: PagedList<PetEntity>?) {
-//    items?.let {
-//        (adapter as PetsAdapter).submitList(items)
-//
-////        (listView.adapter as TasksAdapter).submitList(items)
-//    }
-//}
-//
-//@BindingAdapter("app:goneUnless")
-//fun View.changeVisibility( viewState: ViewState?) {
-//    viewState.takeIf { viewState?.empty !=null }
-//    visibility = if (viewState?.empty) View.VISIBLE else View.GONE
-//}
+import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.paging.PagedList
+import androidx.recyclerview.widget.RecyclerView
+import com.omni.roominkotlinfirsttry.entities.PetEntity
+
+@BindingAdapter("app:payload" ,"app:attachedEmptyView")
+fun RecyclerView.setItems(items: PagedList<PetEntity>?, emptyView: View) {
+    items?.also {
+        emptyView.visibility = if (it.size > 0) View.GONE else View.VISIBLE
+    }.let {
+        (adapter as PetsAdapter).submitList(items)
+    }
+}
