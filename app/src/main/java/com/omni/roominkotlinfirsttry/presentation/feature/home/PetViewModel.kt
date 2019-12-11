b55848761107ee444fff3f6d8a77bfe25da009bb
Omni <engomneya.osman@gmail.com>
2019-12-12 00:25:21 +0200

    clean ViewState

--- app/src/main/java/com/omni/roominkotlinfirsttry/presentation/feature/home/PetViewModel.kt
+++ app/src/main/java/com/omni/roominkotlinfirsttry/presentation/feature/home/PetViewModel.kt
@@ -1,34 +1,21 @@
 package com.omni.roominkotlinfirsttry.presentation.feature.home
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagedList
 import com.omni.roominkotlinfirsttry.domain.repositories.PetsDataSource
 import com.omni.roominkotlinfirsttry.domain.repositories.repository
 import com.omni.roominkotlinfirsttry.entities.PetEntity
-import com.omni.roominkotlinfirsttry.entities.ViewState
 import kotlinx.coroutines.launch
 
 typealias PetsResult = LiveData<PagedList<PetEntity>>
 
-class PetViewModel(
-        val viewState: MutableLiveData<ViewState> = MutableLiveData(),
-        private val petsRepository: PetsDataSource = repository
-) : ViewModel() {
+class PetViewModel(private val petsRepository: PetsDataSource = repository) : ViewModel() {
 
     private var _payload: PetsResult = petsRepository.loadPets()
     val payload: PetsResult = _payload
 
-
-    init {
-        updateViewState(ViewState(loading = true))
-    }
-
-    private fun updateViewState(state: ViewState) {
-        viewState.postValue(state)
-    }
 
     fun savePet(petEntity: PetEntity) {
         petEntity.takeUnless { it.name.isBlank() }
@@ -37,7 +24,6 @@ class PetViewModel(
                         petsRepository.insertAPet(it)
                     }
                 }
-                ?.also { viewState.postValue(ViewState(success = true)) }
     }
 
 
